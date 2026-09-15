10cddd5a9efafefcc0aeb3879a1930faec46c54f
Claire Delmau <clairedelmau9@gmail.com>
2026-09-15 09:55:52 +0200

    Rajout page nos_menus

--- src/Controller/MenusController.php
+++ src/Controller/MenusController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MenuRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,18 +10,13 @@ use Symfony\Component\Routing\Attribute\Route;
 final class MenusController extends AbstractController
 {
     #[Route('/nos_menus', name: 'app_menus')]
-    public function index(): Response
+    public function index(MenuRepository $menuRepository): Response
     {
-        return $this->render('menus/index.html.twig', [
-            'controller_name' => 'MenusController',
-        ]);
-    }
 
-    #[Route('/nos_menus_details', name: 'app_details')]
-    public function details(): Response
-    {
-        return $this->render('menus/details.html.twig', [
-            'controller_name' => 'MenusController',
+        $menus = $menuRepository->findAll();
+
+        return $this->render('menus/index.html.twig', [
+            'menus' => $menus,
         ]);
     }
 
